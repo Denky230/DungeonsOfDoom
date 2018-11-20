2e20083d95be0187ff9f88cc0d309f4b04962a9a
Denky <sorek230@gmail.com>
2018-11-20 20:20:19 +0100

    Added class Hero

--- DungeonsOfDoom/Model/Hero.swift
+++ DungeonsOfDoom/Model/Hero.swift
@@ -11,4 +11,33 @@ import UIKit
 
 class Hero {
     
+    private var name: String
+    private let image: UIImage
+    private var lifes: Int
+    private var exp: Int
+    private var money: Int
+    private let estuf: Estuf
+    
+    init(name: String, image: UIImage, lifes: Int, exp: Int, money: Int, estuf: Estuf) {
+        self.name = name
+        self.image = image
+        self.lifes = lifes
+        self.exp = exp
+        self.money = money
+        self.estuf = estuf
+    }
+    
+    public func getName() -> String { return name }
+    public func setName(newName: String) { self.name = newName }
+    
+    public func getLifes() -> Int { return lifes }
+    public func setLifes(newLifes: Int) { self.lifes = newLifes }
+    
+    public func getExp() -> Int { return exp }
+    public func setExp(newExp: Int) { self.exp = newExp }
+    
+    public func getMoney() -> Int { return money }
+    public func setMoney(newMoney: Int) { self.money = newMoney }
+    
+    public func getEstuf() -> Estuf { return estuf }
 }
