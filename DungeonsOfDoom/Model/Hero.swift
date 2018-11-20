4e1bfa2d78bf758b644b03fbd0063b235a5fe97d
Denky <sorek230@gmail.com>
2018-11-20 19:48:44 +0100

    First commit - Added assets

---
+++ DungeonsOfDoom/Model/Hero.swift
@@ -0,0 +1,14 @@
+//
+//  Hero.swift
+//  DungeonsOfDoom
+//
+//  Created by Oscar Rossello on 20/11/2018.
+//  Copyright © 2018 Oscar Rossello. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Hero {
+    
+}
